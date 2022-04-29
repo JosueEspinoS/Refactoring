2f7dcd975f5195fd0377d105852febb641cf45c2
JosueEspinoS <josue.espino.soto.git@gmail.com>
2022-04-28 19:20:19 -0500

    Paso 12:Corrigiendo método applyValidationInExplorer

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"));
 
 // FizzbuzzService
 const explorer1 = {name: "Explorer1", score: 1}
-FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1)); // {name: "Explorer1", score: 1, trick: 1} 
 
 const explorer3 = {name: "Explorer3", score: 3}
-FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3)); // {name: "Explorer3", score: 3, trick: "FIZZ"}
 
 const explorer5 = {name: "Explorer5", score: 5}
-FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer5)); // {name: "Explorer5", score: 5, trick: "BUZZ"}
 
 const explorer15 = {name: "Explorer15", score: 15}
-FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15)); // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
